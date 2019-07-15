cd02b41ea9577320c8bf924ce27720551b3c615f
Don Knieriem <don@thecanarium.com>
2019-07-14 20:42:55 -0400

    merge Tims work

--- wp-content/themes/understrap-child/loop-templates/post-card.php
+++ wp-content/themes/understrap-child/loop-templates/post-card.php
@@ -1,13 +1,15 @@
-<?php $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), "large"); ?>
-
+<?php $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), "large"); 
+    if ($featuredImage) {
+        $backgroundurl=$featuredImage[0];
+    } else {
+        $backgroundurl=site_url('/wp-content/uploads/2018/09/mom-daughter.jpg');
+    } ?>
     <div class="post-card">
-        <a href="<?php echo esc_url( get_permalink()); ?>"><div class="post-card__heading">
-            <?php if ($featuredImage) { ?>
-                <img src="<?php echo $featuredImage[0];?>">
-            <?php } else { ?>
-                <img src="<?php echo get_site_url();?>/wp-content/uploads/2018/09/mom-daughter.jpg">
-            <?php } ?>  
-            </div></a>
+        <a href="<?php echo esc_url( get_permalink()); ?>">
+            <div class="post-card__heading fill" style="background-image: url('<?php echo $backgroundurl; ?>');">
+                
+            </div>
+        </a>
         <div class="post-card__body">
             <h3 class="post-card__title"><a class="post-card__link" href="<?php echo esc_url( get_permalink()); ?>" ><?php the_title(); ?></a></h3>
             <p class="post-card__text"><?php echo(get_excerpt(100));?></p>
