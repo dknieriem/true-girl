dd7bbfaf0b2e4b22a3454c9e6d78ea5f875a46ea
Don Knieriem <don@thecanarium.com>
2019-07-22 00:24:10 -0400

    revert recaptcha, invalid key

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -144,9 +144,7 @@ function theme_enqueue_styles() {
     wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
     wp_enqueue_script( 'typed', get_stylesheet_directory_uri() . '/js/typed.js', array(), false);
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    wp_enqueue_script('recaptcha-api','https://www.google.com/recaptcha/api.js', false);
     wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/js/custom.js', array(), false);
-    
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
     }
--- wp-content/themes/understrap-child/js/custom.js
+++ wp-content/themes/understrap-child/js/custom.js
@@ -1,1 +1,1 @@
- function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
