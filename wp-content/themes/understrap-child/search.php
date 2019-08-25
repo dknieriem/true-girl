a31c047c1e85cde44c51fc7975dffd79eb5853b7
Don Knieriem <don@thecanarium.com>
2019-08-25 17:06:44 -0400

    search page template

--- wp-content/themes/understrap-child/search.php
+++ wp-content/themes/understrap-child/search.php
@@ -11,20 +11,23 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 ?>
 
-<div class="wrapper" id="index-wrapper">
+<div class="wrapper blog-wrapper" id="index-wrapper">
 
 <div id="content" tabindex="-1"><!--  class="container" -->
 
 	<!-- Page header -->
 	<div class="row">
-		<div class="col-md-12">
-			<h1 class="heading heading--lg heading--white text-center">Search results for "<?php echo get_search_query(); ?>"</h1>
+		<div class="col-md-12 text-center">
+			<h1 class="heading heading--white-on-pink d-inline-block">Search results for "<?php echo get_search_query(); ?>"</h1>
 		</div>
 	</div>
 	<!-- End page header -->
-	<div class="row">
-		<div class="col-md-12"><?php get_search_form();?></div>
+
+	<!-- Search Form -->
+	<div class="row search-page-form">
+		<div class="col-md-6 offset-md-3"><?php get_search_form();?></div>
 	</div>
+	<!-- End Search Form -->
 
 		<?php if ( have_posts() ) : ?>
 
@@ -58,6 +61,18 @@ $container   = get_theme_mod( 'understrap_container_type' );
 	
 
 </div><!-- .row -->
+
+<section class="page-section text-left background-pink responsive-background- " style="background-image: url( '/wp-content/uploads/2019/07/EmailSignupPinkBkgd.png'); background-repeat: no-repeat; background-size:cover; background-position: center center;"><div class="page-section__content-wrapper">
+		
+<div class="row prefooter full-width--no mc-page-form col-sm-10 offset-sm-1 text-center justify-content-center">
+	
+<h2 class="text-white">Truth, delivered to your inbox</h2>
+<?php echo do_shortcode('[mc4wp_form id="7805"]'); ?>
+
+</div>
+
+	</div>
+</section>
 
 </div><!-- Container end -->
 
